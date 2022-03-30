519f01338ad422e035c82cb3b9b472d296635ae6
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2022-03-30 12:00:11 -0300

    atualizando git S12

---
+++ S12_ErudioMicroservices.NET6-AddingProductListingToHome/GeekShooping/GeekShooping.Web/Models/ProductModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeekShopping.Web.Models
+{
+    public class ProductModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageURL { get; set; }
+
+        [Range(1, 100)]
+        public int Count { get; set; } = 1;
+
+        public string SubStringName()
+        {
+            if (Name.Length < 20) return Name;
+            return $"{Name.Substring(0, 17)}...";
+        }
+        public string SubStringDescription()
+        {
+            if (Description.Length < 255) return Description;
+            return $"{Description.Substring(0, 252)}...";
+        }
+    }
+}
