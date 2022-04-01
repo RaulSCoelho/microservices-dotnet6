3efd9493e8cfdecf519746fe6e331b905193d25e
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2022-04-01 13:39:24 -0300

    arrumando nome de pasta

---
+++ S15_ErudioMicroservices.NET6-CreatingShoppingCartUI/GeekShopping/GeekShopping.CartAPI/Data/ValueObjects/CartVO.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace GeekShopping.CartAPI.Data.ValueObjects
+{
+    public class CartVO
+    {
+        [ValidateNever]
+        public CartHeaderVO CartHeader { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<CartDetailVO> CartDetails { get; set; }
+    }
+}
---
+++ S15_ErudioMicroservices.NET6-CreatingShoppingCartUI/GeekShopping/GeekShopping.ProductAPI/Migrations/20220323163221_SeedProductDataTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 3L, "T-Shirt", "Blusa", "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/8_moletom_cobra_kay.jpg?raw=true", "Blusa", 69.900000000000006 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+        }
+    }
+}
